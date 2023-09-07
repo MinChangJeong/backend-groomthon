606e2bb957e958e9d28d82c3af8df5155be326aa
wjdalsckd45 <wjdalsckd45@kakao.com>
2023-09-07 05:48:58 +0000

    test

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `groom` DEFAULT CHARACTER SET utf8mb4;
 
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
+GRANT ALL ON *.* TO 'root'@'*' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL ON groom.* TO 'root'@'*';
 FLUSH PRIVILEGES;
 
 USE `groom`;
