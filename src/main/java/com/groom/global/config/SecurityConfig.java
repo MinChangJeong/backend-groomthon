70bffc89af85a5b8842e56be73f8f7e47c45da07
MinChangJeong <wjdalsckd777@naver.com>
2023-09-07 19:43:56 +0900

    [feat] modify config

--- src/main/java/com/groom/global/config/SecurityConfig.java
+++ src/main/java/com/groom/global/config/SecurityConfig.java
@@ -3,6 +3,11 @@ package com.groom.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,22 +19,44 @@ import java.util.List;
  * Spring Security 설정 빈
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin();
+        // cors 설정
+        http.cors().configurationSource(corsConfigurationSource());
+        // 세션 사용 x
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable()
+                .httpBasic().disable() // http basic 인증 방식 사용 x
+                .formLogin().disable() // form login 인증 방식 사용 x
+                .logout().disable();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
 
-    /**
-     * Cors 설정
-     */
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
-        configuration.setAllowedHeaders(List.of("*"));
+        // 모든 origin 허용
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Refresh"));
+        configuration.setMaxAge(3600L);
         configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+        return urlBasedCorsConfigurationSource;
     }
 }
+
+
+
+
+
